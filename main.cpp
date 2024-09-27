ca266d6bf83a4abb306264d88e63270ae47703ee
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-09-27 10:23:13 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -174,7 +174,33 @@ int month_days(int month, int year){
     }
 
 }
-
+string dayname(int n){
+    switch(n){
+        case 0:     return "Sunday";
+        case 1:     return "Monday";
+        case 2:     return "Tuesday";
+        case 3:     return "Wednesday";
+        case 4:     return "Thursday";
+        case 5:     return "Friday";
+        default:    return "Saturday";
+    }
+}
+string monthname(int n){
+    switch(n){
+        case 0:     return "January";
+        case 1:     return "February";
+        case 2:     return "March";
+        case 3:     return "April";
+        case 4:     return "May";
+        case 5:     return "June";
+        case 6:    return "July";
+        case 7:    return "August";
+        case 8:    return "September";
+        case 9:    return "October";
+        case 6:    return "November";
+        default:    return "December"
+    }
+}
 void problem24(){
    int y, d;
    cin >> y >> d;
